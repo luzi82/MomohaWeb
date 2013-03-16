423578106c05544f2ad79785f96dbc0a75ce7c01
luzi82 <luzi82@gmail.com>
2013-03-17 01:21:43 +0800

    simple data structure

--- src/Feed/models.py
+++ src/Feed/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+URL_LENGTH = 1024
+TITLE_LENGTH = 128
+
+Subscription_CONTENT_STATE_INVISIBLE = 0
+Subscription_CONTENT_STATE_UNREAD = 1
+Subscription_CONTENT_STATE_READ = 2
+
+class Feed(models.Model):
+
+    url = models.CharField(max_length=URL_LENGTH,db_index=True)
+    title = models.CharField(max_length=TITLE_LENGTH)
+
+
+class Subscription(models.Model):
+
+    user = models.ForeignKey(auth_models.User,db_index=True)
+    feed = models.ForeignKey(Feed,db_index=True)
+
+
+class FeedContent(models.Model):
+    
+    feed = models.ForeignKey(Feed,db_index=True)
+    title = models.CharField(max_length=TITLE_LENGTH)
+
+
+class SubscriptionContent(models.Model):
+    
+    content = models.ForeignKey(FeedContent,db_index=True)
+    Subscription = models.ForeignKey(Subscription,db_index=True)
+    state = models.IntegerField(db_index=True)
