42b5e2674374b8f4ef269469866e9ef6f1f9bf80
luzi82 <luzi82@gmail.com>
2013-03-23 21:24:19 +0800

    add more commands

--- src/Feed/views.py
+++ src/Feed/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"index.html")
+
+def addSubscription(request):
+    pass
+
+def rmSubscription(request):
+    pass
+
+def listSubscription(request):
+    pass
+
+def listSubscriptionContent(request):
+    pass
+
+def markRead(request):
+    pass
