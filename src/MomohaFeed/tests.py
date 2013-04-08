a0320b8460a81b5df8ebcea6b64777e9fc71867a
luzi82 <luzi82@gmail.com>
2013-04-08 23:57:18 +0800

    add test test_update_feed_pool

--- src/MomohaFeed/tests.py
+++ src/MomohaFeed/tests.py
@@ -10,6 +10,7 @@ import os
 from threading import Thread
 import MomohaFeed
 import feedparser
+import time
 
 MY_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -481,6 +482,63 @@ class SimpleTest(TestCase):
         result = simplejson.loads(content)
 
         self.assertEqual(True, result['success'])
+
+        response = client.post("/feed/j_subscription_list_item/",{
+            'subscription_id': subscription_id
+        })
+        content=response.content
+        result = simplejson.loads(content)
+
+        vm_item = result['item_list'][2]
+        self.assertEqual(u'馬道立：人大常委會解釋基本法　法院受約束', vm_item['title'])
+
+
+    def test_update_feed_pool (self):
+        
+        TMP_HTTP_PORT = SimpleTest.TMP_HTTP_PORT
+        TEST_URL = 'http://localhost:{0}/test.xml'.format(TMP_HTTP_PORT)
+
+        httpServer = memhttpserver.MemHTTPServer(('localhost',TMP_HTTP_PORT))
+        httpServer.server_activate()
+
+        User.objects.create_user("user",password="pass")
+        
+        client = Client()
+        client.login(username="user",password="pass")
+        
+        feed = open(MY_DIR+"/test/yahoo_hk_rss_0.xml").read()
+        httpServer.set_get_output('/test.xml', 'text/rss', feed)
+
+        thread = Thread(target=httpServer.handle_request)
+        thread.start()
+
+        response = client.post("/feed/j_add_subscription/",{
+            'url':TEST_URL
+        })
+        content=response.content
+        result = simplejson.loads(content)
+
+        self.assertEqual(True, result['success'])
+        subscription_id = result['subscription']['id']
+        
+        response = client.post("/feed/j_subscription_list_item/",{
+            'subscription_id': subscription_id
+        })
+        content=response.content
+        result = simplejson.loads(content)
+
+        vm_item = result['item_list'][2]
+        self.assertEqual(u'將軍澳醫院疑似H7N9個案測試結果呈陰性', vm_item['title'])
+
+        feed = open(MY_DIR+"/test/yahoo_hk_rss_1.xml").read()
+        httpServer.set_get_output('/test.xml', 'text/rss', feed)
+        
+        time.sleep(1500.0/1000.0)
+
+        thread = Thread(target=httpServer.handle_request)
+        thread.start()
+
+        MomohaFeed.update_feed_pool(1000)
 
         response = client.post("/feed/j_subscription_list_item/",{
             'subscription_id': subscription_id
