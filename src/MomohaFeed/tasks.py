fdbc5ae3f421fe3a9b43fd7d28cf05d9cdebe388
luzi82 <luzi82@gmail.com>
2013-04-09 22:06:39 +0800

    periodic_update_feed_pool

---
+++ src/MomohaFeed/tasks.py
@@ -0,0 +1,24 @@
+from celery.schedules import crontab
+from celery.task import periodic_task
+from django.core.cache import cache
+
+import logging
+import MomohaFeed
+
+logger = logging.getLogger(__name__)
+
+@periodic_task(run_every=crontab())
+def periodic_update_feed_pool():
+    
+    LOCK_ID = "periodic_update_feed_pool"
+    LOCK_EXPIRE = 60 * 5 # Lock expires in 5 minutes
+    UPDATE_TIMEOUT = 15*60*1000
+    
+    acquire_lock = lambda: cache.add(LOCK_ID, 'true', LOCK_EXPIRE)
+    release_lock = lambda: cache.delete(LOCK_ID)
+
+    if acquire_lock():
+        try:
+            MomohaFeed.update_feed_pool(UPDATE_TIMEOUT)
+        finally:
+            release_lock()
