6dfb42950376c0f2223aa1af8e13db3cebffaa22
luzi82 <luzi82@gmail.com>
2013-04-20 22:49:16 +0800

    improve test script

--- dev_test.sh
+++ dev_test.sh
@@ -7,6 +7,8 @@ cd src
 if [ "$#" = "0" ]; then
 	python manage.py test MomohaFeed
 	python manage.py test KyubeyUser
+elif [ "$#" = "1" ]; then
+	python manage.py test ${1}
 elif [ "$#" = "2" ]; then
 	python manage.py test ${1}.SimpleTest.${2}
 fi
