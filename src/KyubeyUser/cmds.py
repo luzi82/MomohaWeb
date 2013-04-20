3991c91c951dad63f5aef76ae3d5ffec3a74820d
luzi82 <luzi82@gmail.com>
2013-04-20 18:48:11 +0800

    #35 add_user login logout user_set_password

--- src/KyubeyUser/cmds.py
+++ src/KyubeyUser/cmds.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import PermissionDenied
 import inspect
+from django.contrib.auth.models import User
+import django.contrib.auth
 
 cmd_list = []
 cmd_dict = {}
@@ -21,3 +23,54 @@ def u403(f):
             raise PermissionDenied
         return f(request,*args,**kwargs)
     return ff
+
+
+###########
+
+@cmd
+def add_user(request, username, password):
+    User.objects.create_user(
+        username = username ,
+        password = password ,
+    )
+    user = django.contrib.auth.authenticate(
+        username = username,
+        password = password
+    )
+    if user is not None:
+        if user.is_active:
+            django.contrib.auth.login(request, user)
+            return {'success':True}
+        return {'success':False, 'resaon':"user not active"}
+    return {'success':False, 'reason':'create user fail'}
+
+
+@cmd
+def login(request, username, password):
+    user = django.contrib.auth.authenticate(
+        username = username,
+        password = password
+    )
+    if user is not None:
+        if user.is_active:
+            django.contrib.auth.login(request, user)
+            return {'success':True}
+        return {'success':False, 'resaon':"user not active"}
+    return {'success':False, 'reason':'user not exist'}
+
+
+@u403
+@cmd
+def logout(request):
+    django.contrib.auth.logout(request)
+    return {'success':True}
+
+
+@u403
+@cmd
+def user_set_password(request, old_password, new_password):
+    if not request.user.check_password(old_password):
+        return {'success':False, 'reason':'old_password not correct'}
+    request.user.set_password(new_password)
+    request.user.save()
+    return {'success':True}
--- src/KyubeyUser/tests.py
+++ src/KyubeyUser/tests.py
@@ -1,16 +1,172 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import Client
+import simplejson
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    
+    TEST_USERNAME = 'Kyubey'
+    TEST_PASSWORD = 'incubator'
+
+    def test_reg_acc(self):
+        """MomohaWeb issue 35: json reg account"""
+        
+        client = Client()
+        
+        self.assertNotIn('_auth_user_id', client.session)
+        
+        response = client.post("/users/json/",{'json':simplejson.dumps({
+            'cmd':'add_user',
+            'argv':{
+                'username': SimpleTest.TEST_USERNAME,
+                'password': SimpleTest.TEST_PASSWORD,
+            },
+        })})
+        self.assertEqual(200, response.status_code)
+        content = response.content
+        result = simplejson.loads(content)
+        
+        self.assertIn('success', result)
+        self.assertTrue(result['success'])
+        self.assertIn('_auth_user_id', client.session)
+
+
+    def test_login(self):
+
+        client = Client()
+        
+        response = client.post("/users/json/",{'json':simplejson.dumps({
+            'cmd':'add_user',
+            'argv':{
+                'username': SimpleTest.TEST_USERNAME,
+                'password': SimpleTest.TEST_PASSWORD,
+            },
+        })})
+        self.assertEqual(200, response.status_code)
+        content = response.content
+        result = simplejson.loads(content)
+        
+        self.assertTrue(result['success'])
+
+
+        client = Client()
+        
+        self.assertNotIn('_auth_user_id', client.session)
+
+        response = client.post("/users/json/",{'json':simplejson.dumps({
+            'cmd':'login',
+            'argv':{
+                'username': SimpleTest.TEST_USERNAME,
+                'password': SimpleTest.TEST_PASSWORD,
+            },
+        })})
+        self.assertEqual(200, response.status_code)
+        content = response.content
+        result = simplejson.loads(content)
+
+        self.assertIn('success', result)
+        self.assertTrue(result['success'])
+        self.assertIn('_auth_user_id', client.session)
+
+
+    def test_logout(self):
+        
+        client = Client()
+        
+        self.assertNotIn('_auth_user_id', client.session)
+        
+
+        response = client.post("/users/json/",{'json':simplejson.dumps({
+            'cmd':'add_user',
+            'argv':{
+                'username': SimpleTest.TEST_USERNAME,
+                'password': SimpleTest.TEST_PASSWORD,
+            },
+        })})
+        self.assertEqual(200, response.status_code)
+        content = response.content
+        result = simplejson.loads(content)
+        
+        self.assertTrue(result['success'])
+
+
+        response = client.post("/users/json/",{'json':simplejson.dumps({
+            'cmd':'logout',
+        })})
+        self.assertEqual(200, response.status_code)
+        content = response.content
+        result = simplejson.loads(content)
+
+        self.assertTrue(result['success'])
+        self.assertNotIn('_auth_user_id', client.session)
+
+
+    def test_user_set_password(self):
+        
+        password2 = SimpleTest.TEST_PASSWORD+'x'
+        
+        client = Client()
+        
+        self.assertNotIn('_auth_user_id', client.session)
+        
+
+        response = client.post("/users/json/",{'json':simplejson.dumps({
+            'cmd':'add_user',
+            'argv':{
+                'username': SimpleTest.TEST_USERNAME,
+                'password': SimpleTest.TEST_PASSWORD,
+            },
+        })})
+        self.assertEqual(200, response.status_code)
+        content = response.content
+        result = simplejson.loads(content)
+        
+        self.assertTrue(result['success'])
+
+
+        response = client.post("/users/json/",{'json':simplejson.dumps({
+            'cmd':'user_set_password',
+            'argv':{
+                'old_password': SimpleTest.TEST_PASSWORD,
+                'new_password': password2,
+            },
+        })})
+        self.assertEqual(200, response.status_code)
+        content = response.content
+        result = simplejson.loads(content)
+        
+        self.assertTrue(result['success'])
+
+
+        client = Client()
+        self.assertNotIn('_auth_user_id', client.session)
+
+
+        response = client.post("/users/json/",{'json':simplejson.dumps({
+            'cmd':'login',
+            'argv':{
+                'username': SimpleTest.TEST_USERNAME,
+                'password': SimpleTest.TEST_PASSWORD,
+            },
+        })})
+        self.assertEqual(200, response.status_code)
+        content = response.content
+        result = simplejson.loads(content)
+        
+        self.assertFalse(result['success'])
+
+
+        response = client.post("/users/json/",{'json':simplejson.dumps({
+            'cmd':'login',
+            'argv':{
+                'username': SimpleTest.TEST_USERNAME,
+                'password': password2,
+            },
+        })})
+        self.assertEqual(200, response.status_code)
+        content = response.content
+        result = simplejson.loads(content)
+        
+        self.assertIn('success', result)
+        self.assertTrue(result['success'])
+        self.assertIn('_auth_user_id', client.session)
