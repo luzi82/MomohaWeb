852d6835e58187058f698ad77cc2e084b79b62e8
luzi82 <luzi82@gmail.com>
2013-04-23 20:17:17 +0800

    login UI, disable input, show progress bar #68

--- src/static/users/ui.js
+++ src/static/users/ui.js
@@ -17,10 +17,17 @@ define([
 			$('#users_auth_login_btn').click(function(){
 				var username = $('#users_auth_login_username').val();
 				var password = $('#users_auth_login_password').val();
+				$("#users_auth_login_username").prop('disabled', true);
+				$("#users_auth_login_password").prop('disabled', true);
+				$('#users_auth_login_btn').prop('disabled', true);
+				users_auth_login_progress(30);
 				users.login(username, password, function(success, reason){
 					console.log("success "+success);
 					if(!success){
+						// TODO
+						return;
 					}
+					users_auth_login_progress(100);
 				});
 			});
 			
@@ -31,8 +38,19 @@ define([
 	var show_login = function(){
 		console.log("show_login");
 		$(".app").hide();
+		
 		$("#users_auth").show();
+		$("#users_auth_login_progress").hide();
+		$("#users_auth_login_username").prop('disabled', false);
+		$("#users_auth_login_password").prop('disabled', false);
+		$('#users_auth_login_btn').prop('disabled', false);
+		
 		$("#users_app").show();
+	};
+	
+	var users_auth_login_progress = function(val){
+		$("#users_auth_login_progress").show();
+		$("#users_auth_login_progress_bar").css("width",""+val+"%");
 	};
 	
 	init();
