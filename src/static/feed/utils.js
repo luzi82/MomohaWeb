9f21e32e9589c5217c0eb99d6853e38a6279efde
luzi82 <luzi82@gmail.com>
2013-04-26 00:15:18 +0800

    remove unused func

--- src/static/feed/utils.js
+++ src/static/feed/utils.js
@@ -10,23 +10,6 @@ define([
 		}
 	}
 	
-	var remote = function(cmd,argv,callback){
-		if(argv==null)argv={};
-		if(callback==null)callback=function(){};
-		$.ajax({
-			type: "POST",
-			dataType: "json",
-			url: "/feed/json/",
-			data: {
-				csrfmiddlewaretoken: $.cookie('csrftoken'),
-				json: JSON.stringify({
-					cmd: cmd,
-					argv: argv,
-				}),
-			},
-		}).done(callback);
-	}
-		
 	return {
 		cb: cb,
 		remote: remote,
