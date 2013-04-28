2f22ab48817c6958ff207bb3d595a49a0f64c33f
luzi82 <luzi82@gmail.com>
2013-04-28 10:22:43 +0800

    update test script

--- dev_test.sh
+++ dev_test.sh
@@ -5,8 +5,9 @@
 cd src
 
 if [ "$#" = "0" ]; then
-	python manage.py test MomohaFeed
-	python manage.py test KyubeyAuth
+	#python manage.py test --failfast || exit 1
+	python manage.py test --failfast MomohaFeed || exit 1
+	python manage.py test --failfast KyubeyAuth || exit 1
 elif [ "$#" = "1" ]; then
 	python manage.py test ${1}
 elif [ "$#" = "2" ]; then
