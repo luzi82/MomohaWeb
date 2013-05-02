1fceb009fb4447933fb0859b6b35e54332f04d4d
luzi82 <luzi82@gmail.com>
2013-05-02 19:58:56 +0800

    prevent staff load from cache

--- src/static/main.js
+++ src/static/main.js
@@ -1,5 +1,6 @@
 requirejs.config({
     baseUrl: 'inc',
+    urlArgs: "bust=" + (new Date()).getTime(),
     paths: {
         jquery: 'jquery/jquery-1.9.1',
         bootstrap: 'bootstrap/js/bootstrap',
@@ -29,51 +30,61 @@ requirejs.config({
 
 require([
 	"jquery",
-	"auth",
-	"auth_ui",
-	"feed_root_layout",
-	"feed_ui",
-	
-	"bootstrap",
-	"cookie",
-	"json",
-
-	"feed_utils",
-	"feed_add_subscription",
-	"feed_list_subscription",
-	"feed_subscription",
-	"feed_share",
-	
-	"momohafeed",
 ], function(
 	$
-	, auth
-	, auth_ui
-	, feed_root_layout
-	, feed_ui
 ) {
-	console.log("main");
 	
-	var login_true = function(){
-		feed_ui.show();
-	};
-	
-	var login_false = function(){
-		feed_ui.hide();
-		auth_ui.show_login();
-	};
-	
-	$("body").on("login_done",login_true);
-	$("body").on("start_verify_login_done",login_true);
-	$("body").on("logout_done",login_false);
-	$("body").on("start_verify_login_fail",login_false);
-	
-	auth.verify_login(function(success){
-		// console.log("verify_login: "+success);
-		if(success){
-			$("body").trigger("start_verify_login_done");
-		}else{
-			$("body").trigger("start_verify_login_fail");
-		}
+	$.ajaxSetup ({
+	    cache: false
 	});
+
+	require([
+		"auth",
+		"auth_ui",
+		"feed_root_layout",
+		"feed_ui",
+		
+		"bootstrap",
+		"cookie",
+		"json",
+	
+		"feed_utils",
+		"feed_add_subscription",
+		"feed_list_subscription",
+		"feed_subscription",
+		"feed_share",
+		
+		"momohafeed",
+	], function(
+		auth
+		, auth_ui
+		, feed_root_layout
+		, feed_ui
+	) {
+		console.log("main");
+		
+		var login_true = function(){
+			feed_ui.show();
+		};
+		
+		var login_false = function(){
+			feed_ui.hide();
+			auth_ui.show_login();
+		};
+		
+		$("body").on("login_done",login_true);
+		$("body").on("start_verify_login_done",login_true);
+		$("body").on("logout_done",login_false);
+		$("body").on("start_verify_login_fail",login_false);
+		
+		auth.verify_login(function(success){
+			// console.log("verify_login: "+success);
+			if(success){
+				$("body").trigger("start_verify_login_done");
+			}else{
+				$("body").trigger("start_verify_login_fail");
+			}
+		});
+	});
+
 });
